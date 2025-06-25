d5a07e0342ff23bd7b830af3bd0890f3b62c16be
Ankit Kumar <ankit@creditoracademy.com>
2025-06-25 20:08:20 +0530

    updates

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,6 +10,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
   "http://127.0.0.1:5500/docs" ,
+  "https://ankitcreditor.github.io/real-estate/",
   // Optional, include if you're serving from here
 ];
 
